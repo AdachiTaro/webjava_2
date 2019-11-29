7b0c50ba3570c072a984308f97d4d78f2bb26d94
adachita <adachita@systena.co.jp>
2019-11-30 05:45:29 +0900

    ⑤完成

--- src/main/java/jp/co/systena/tigerscave/springtest/controller/RpgController.java
+++ src/main/java/jp/co/systena/tigerscave/springtest/controller/RpgController.java
@@ -33,7 +33,7 @@ public class RpgController {
     int id = 1;
     if (session.getAttribute("partyList") != null) {
       mParty = (Party) session.getAttribute("partyList");
-      id = mParty.getPartyList().size();
+      id = mParty.getPartyList().size() +1;
     }
     final int DEFAULT_HP = 100;
     Character createdCharacter = new Character(id,characterCreateForm.getJob(),DEFAULT_HP,characterCreateForm.getName());
@@ -54,15 +54,50 @@ public class RpgController {
   public ModelAndView commandSelect(HttpSession session, ModelAndView mav) {
     mParty = (Party)session.getAttribute("partyList");
 
-
-
     mav.addObject("party", mParty);
     mav.setViewName("CommandView");
     return mav;
   }
 
+  @RequestMapping(value = "/SelectCommand", method = RequestMethod.POST)
+  public ModelAndView commandSelect(@ModelAttribute("selectcommand") String selectedCommand,@ModelAttribute("memberId") int selectCharaId, HttpSession session, ModelAndView mav) {
+    mParty = (Party)session.getAttribute("partyList");
+    List<Character> partyList = mParty.getPartyList();
+
+    // 選ばれたコマンドの表示する文言をキャラクタにセット
+    Character commandSelectChara = partyList.get(selectCharaId -1);
+    switch (selectedCommand) {
+      case "たたかう" :
+        commandSelectChara.setCommand(commandSelectChara.getJob().fight());
+        break;
+      case "かいふく" :
+        commandSelectChara.setCommand(commandSelectChara.getJob().heal());
+        break;
+    }
+
+    // 全員のコマンド選択が終わっているか確認
+    Party commandUnselectedCharacterList = new Party();
+    for (Character chara : mParty.getPartyList()) {
+      if (chara.getCommand() == "未選択") {
+        commandUnselectedCharacterList.addPartyMember(chara);
+      }
+    }
+
+    if (commandUnselectedCharacterList.getPartyList().size() != 0) {
+      // コマンド選択が終わっていないキャラがいるため、未選択キャラだけ残したCommandViewを再表示
+      mav.addObject("party", commandUnselectedCharacterList);
+      mav.setViewName("CommandView");
+    } else {
+      // コマンド選択が終わっているようなので戦闘結果画面を表示
+      mav.addObject("party", mParty);
+      mav.setViewName("BattleView");
+    }
+    return mav;
+  }
+
   @RequestMapping(value = "/Battle", method = RequestMethod.POST)
   public ModelAndView battleStart(@ModelAttribute("battle") String selectedCommand, HttpSession session, ModelAndView mav) {
+    // TODO:ここのメソッドは戦闘画面ように直す予定
     mParty = (Party)session.getAttribute("partyList");
     List<Character> partyList = mParty.getPartyList();
     for (Character member:partyList) {
